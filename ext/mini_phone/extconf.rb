046479b4b9f16ba85684ef68cc6b22cb4fafcfa3
Scott Hetrick <scotthetrick2@yahoo.com>
2023-03-28 13:17:04 -0700

    Cleaning up the fix

--- ext/mini_phone/extconf.rb
+++ ext/mini_phone/extconf.rb
@@ -12,23 +12,23 @@ if conf['target_cpu'] == 'arm64' && conf['target_os'].start_with?('darwin')
   $CXXFLAGS << ' -I/opt/homebrew/include '
 end
 
-unless have_library("phonenumber")
+unless have_library("phonenumber") && have_library("protobuf")
   abort <<~MSG
 
     ,----------------------------------------------------------------------,
     |                                                                      |
     | Hey there, sorry for the inconvenience!                              |
     |                                                                      |
-    | It looks like libphonenumber is not installed on this system. Don't  |
-    | worry, this should be easy to fix:                                   |
+    | It looks like libphonenumber and/or protobuf is not installed on     |
+    | this system. Don't worry, this should be easy to fix:                |
     |                                                                      |
-    | 1. Install the lib                                                   |
+    | 1. Install the libs                                                  |
     |                                                                      |
     |  On Mac:                                                             |
-    |    brew install libphonenumber                                       |
+    |    brew install libphonenumber protobuf                              |
     |                                                                      |
     |  On Debian / Ubuntu:                                                 |
-    |    apt-get install -y libphonenumber-dev                             |
+    |    apt-get install -y libphonenumber-dev libprotobuf-dev             |
     |                                                                      |
     | 2. Retry installing the gem (i.e `bundle install`)                   |
     |                                                                      |
@@ -40,8 +40,6 @@ end
 dir_config('mini_phone')
 
 $CXXFLAGS += ' -std=c++17 -ofast '
-$LDFLAGS << " -L/usr/local/opt/protobuf/lib"
-$CPPFLAGS << " -I/usr/local/opt/protobuf/include"
 
 create_makefile('mini_phone/mini_phone')
 
